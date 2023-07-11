38e2c2910b5b183f5c4e9cc5dae31a8d6c9c20d8
Blu369 <llacuna.darwin24@gmail.com>
2023-07-12 00:08:02 +0800

    Creating Schema, Models And create POST function

--- Backend/routes/workouts.js
+++ Backend/routes/workouts.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router();
+const Workout = require('../models/Workout')
 
 // GET FUNCTION
 router.get('/', (req, res) => {
@@ -11,7 +12,14 @@ router.get('/:id', (req, res) => {
     res.json({msg: 'GET a single workout'})
 })
 //POST FUNCTION
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    const {title, load, reps} = req.body
+    try{
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    } catch(error){
+        res.status(400).json({error: error.message})
+    }
     res.json({msg: 'POST a new Workout'})
 })
 //DELETE function
