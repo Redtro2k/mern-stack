633a23939cba5bc5f32c60e77b668d20508996d0
Blu369 <llacuna.darwin24@gmail.com>
2023-07-11 16:39:17 +0800

    add middleware

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,12 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 
+//middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
 //routes
 app.get('/', (req, res) => {
     res.json({msg: 'Welcome to the App'})
