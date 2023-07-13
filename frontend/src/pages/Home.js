6cd79bd0e1f0936128f2254b5807f9bab750a9a6
Blu369 <llacuna.darwin24@gmail.com>
2023-07-13 11:16:58 +0800

    fetching data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,26 @@
+import {useEffect, useState} from 'react'
+import WorkoutDetails from '../components/WorkoutDetails'
+
 const Home = () => {
+    const [workouts, setworkouts] = useState(null);
+    useEffect(() => {
+        const fetchworkouts = async () => {
+            const response = await  fetch('/api/workouts')
+            const json = await response.json()
+            if(response.ok){
+                setworkouts(json)
+            }
+        }
+        fetchworkouts()
+    }, [])
     return (
         <div className="home">
             <h2>Home</h2>
+            <div className='workouts'>
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
         </div>
     )
 } 
